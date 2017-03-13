97fc426753c9ec4927628045c21ca93b9d755602
Iverson <Dan.Iverson@minneapolismn.gov>
2017-03-13 14:54:18 -0500

    Script changes from testing. Modified CLASSPATH and added  instead of e:\psoft\ELM92TST\sdk

--- setup/util-compile.ps1
+++ setup/util-compile.ps1
@@ -20,9 +20,14 @@ Function Invoke-CmdScript([string] $script, [string] $parameters) {
 #$Env:PS_PIA_HOME
 #$Env:WL_HOME
 #$Env:UTIL_DIR
+#$Env:WLSADMIN="system"
+#$Env:WLSPASS="Passw0rd"
+#$Env:WLSCONN="t3://localhost:8000"
+#$Env:ELF_FIELDS="date time cs-method cs-uri sc-status cs-username cs(user-agent) s-ip c-ip time-taken bytes cs(X-Forwarded-For)"
+
 
 # compile
-Invoke-CmdScript "$Env:WL_HOME\server\bin\setWLSEnv.cmd"
-$Env:CLASSPATH+="$Env:PS_PIA_HOME\applications\peoplesoft\PORTAL.war\WEB-INF\classes;"
-[Void][System.IO.Directory]::CreateDirectory("$Env:PS_CUST_HOME\class") 
+Invoke-CmdScript "${Env:PS_PIA_HOME}\bin\setEnv.cmd"
+$Env:CLASSPATH="${Env:PS_PIA_HOME}\applications\peoplesoft\PORTAL.war\WEB-INF\classes${env:CLASSPATH}"
+[Void][System.IO.Directory]::CreateDirectory("${Env:PS_CUST_HOME}\class") 
 javac $(Get-ChildItem -Path $Env:UTIL_DIR\src\main\java -Filter *.java -Recurse | % {$_.Fullname}) -d $Env:PS_CUST_HOME\class
